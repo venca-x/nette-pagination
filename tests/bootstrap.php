02be60c5564289f689d19ba40f102d46bbe247b8
vEnCa-X <venca-x@seznam.cz>
2017-07-26 20:41:51 +0200

    fix test?

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,21 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-
+/*
 (new \Nette\Loaders\RobotLoader())
 	->setCacheStorage(new \Nette\Caching\Storages\FileStorage(__DIR__ . '/temp'))
 	->addDirectory([__DIR__ . '/../src'])
 	->register();
+*/
+if (!class_exists('Tester\Assert')) {
+    echo "Install Nette Tester using `composer update --dev`\n";
+    exit(1);
+}
 
+/*
 Testbench\Bootstrap::setup(__DIR__ . '/temp', function (\Nette\Configurator $configurator) {
 	$configurator->addParameters([
 		'appDir' => __DIR__ . '/../src',
 	]);
-
-	//$configurator->addConfig(__DIR__ . '/../app/config/config.neon');
 });
+*/
